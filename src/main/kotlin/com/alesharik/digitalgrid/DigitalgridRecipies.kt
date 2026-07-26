5ef61d6a0c345e2834cde6bb1b665c4b8e5e781a
alesharik <alesharik4@gmail.com>
2026-07-26 17:10:54 +0300

    refactor: cleanup recipe APIs

--- src/main/kotlin/com/alesharik/digitalgrid/DigitalgridRecipies.kt
+++ src/main/kotlin/com/alesharik/digitalgrid/DigitalgridRecipies.kt
@@ -1,20 +1,22 @@
 package com.alesharik.digitalgrid
 
+import com.alesharik.digitalgrid.recipe.BreadboardingAttachRecipe
+import com.alesharik.digitalgrid.recipe.BreadboardingCraftRecipe
+import com.alesharik.digitalgrid.recipe.CountedIngredient
+import com.alesharik.digitalgrid.utils.PressingRecipeGenKt
+import com.alesharik.digitalgrid.utils.SequencedAssemblyRecipeGenKt
 import com.alesharik.digitalgrid.utils.shaped
 import com.alesharik.digitalgrid.utils.shapeless
 import com.simibubi.create.AllItems
-import com.simibubi.create.api.data.recipe.PressingRecipeGen
-import com.simibubi.create.api.data.recipe.SequencedAssemblyRecipeGen
-import com.simibubi.create.content.kinetics.deployer.DeployerApplicationRecipe
-import com.simibubi.create.content.kinetics.deployer.ItemApplicationRecipe
-import com.simibubi.create.content.kinetics.press.PressingRecipe
-import com.simibubi.create.content.processing.recipe.StandardProcessingRecipe
 import dan200.computercraft.shared.ModRegistry
 import net.minecraft.core.HolderLookup
 import net.minecraft.data.PackOutput
 import net.minecraft.data.recipes.RecipeOutput
 import net.minecraft.data.recipes.RecipeProvider
+import net.minecraft.resources.ResourceLocation
+import net.minecraft.world.item.ItemStack
 import net.minecraft.world.item.Items
+import net.minecraft.world.item.crafting.Ingredient
 import net.neoforged.neoforge.data.event.GatherDataEvent
 import org.patryk3211.powergrid.collections.ModdedBlocks
 import org.patryk3211.powergrid.collections.ModdedItems
@@ -30,44 +32,31 @@ object DigitalgridRecipies {
     }
 
     class PressingRecipes(out: PackOutput, registries: CompletableFuture<HolderLookup.Provider>) :
-        PressingRecipeGen(
+        PressingRecipeGenKt(
             out,
             registries,
             Digitalgrid.ID
         ) {
-        val PLASTIC = create("plastic", {
-            it.require(AllItems.CARDBOARD).output(DigitalgridRegistry.Items.PLASTIC)
-        })
+
+        val PLASTIC = create("plastic") {
+            require(AllItems.CARDBOARD).output(DigitalgridRegistry.Items.PLASTIC)
+        }
     }
 
     class SequencedAssemblyRecipes(out: PackOutput, registries: CompletableFuture<HolderLookup.Provider>) :
-        SequencedAssemblyRecipeGen(out, registries, Digitalgrid.ID) {
-        val DC_DC_CONVERTER = create("dc_dc_converter", { b ->
-            b.require(ModdedItems.EMPTY_CIRCUIT)
+        SequencedAssemblyRecipeGenKt(out, registries, Digitalgrid.ID) {
+
+        val DC_DC_CONVERTER = create("dc_dc_converter") {
+            require(ModdedItems.EMPTY_CIRCUIT)
                 .transitionTo(DigitalgridRegistry.Items.INCOMPLETE_DC_DC_CONVERTER)
                 .addOutput(DigitalgridRegistry.Items.DC_DC_CONVERTER, 100f)
                 .loops(1)
-                .addStep(
-                    ItemApplicationRecipe.Factory { params -> DeployerApplicationRecipe(params) },
-                    { it.require(ModdedItems.DIODE) },
-                )
-                .addStep(
-                    ItemApplicationRecipe.Factory { params -> DeployerApplicationRecipe(params) },
-                    { it.require(ModdedItems.CAPACITOR) },
-                )
-                .addStep(
-                    ItemApplicationRecipe.Factory { params -> DeployerApplicationRecipe(params) },
-                    { it.require(ModdedItems.COPPER_COIL) },
-                )
-                .addStep(
-                    ItemApplicationRecipe.Factory { params -> DeployerApplicationRecipe(params) },
-                    { it.require(DigitalgridTags.Items.PLASTICS) },
-                )
-                .addStep(
-                    StandardProcessingRecipe.Factory { params -> PressingRecipe(params) },
-                    { it },
-                )
-        })
+                .addDeployerStep(ModdedItems.DIODE)
+                .addDeployerStep(ModdedItems.CAPACITOR)
+                .addDeployerStep(ModdedItems.COPPER_COIL)
+                .addDeployerStep(DigitalgridTags.Items.PLASTICS)
+                .addPressStep()
+        }
     }
 
     class CraftingRecipies(out: PackOutput, registries: CompletableFuture<HolderLookup.Provider>) :
--- src/main/kotlin/com/alesharik/digitalgrid/utils/Recipe.kt
+++ src/main/kotlin/com/alesharik/digitalgrid/utils/Recipe.kt
@@ -1,17 +1,73 @@
 package com.alesharik.digitalgrid.utils
 
+import com.simibubi.create.api.data.recipe.PressingRecipeGen
+import com.simibubi.create.api.data.recipe.SequencedAssemblyRecipeGen
+import com.simibubi.create.content.kinetics.deployer.DeployerApplicationRecipe
+import com.simibubi.create.content.kinetics.deployer.ItemApplicationRecipe
+import com.simibubi.create.content.kinetics.press.PressingRecipe
+import com.simibubi.create.content.processing.recipe.StandardProcessingRecipe
+import com.simibubi.create.content.processing.sequenced.SequencedAssemblyRecipeBuilder
+import net.minecraft.core.HolderLookup
+import net.minecraft.data.PackOutput
 import net.minecraft.data.recipes.RecipeCategory
 import net.minecraft.data.recipes.RecipeOutput
 import net.minecraft.data.recipes.ShapedRecipeBuilder
 import net.minecraft.data.recipes.ShapelessRecipeBuilder
+import net.minecraft.tags.TagKey
+import net.minecraft.world.item.Item
 import net.minecraft.world.level.ItemLike
+import java.util.concurrent.CompletableFuture
 
-inline fun RecipeOutput.shaped(item: ItemLike, count: Int, crossinline f: ShapedRecipeBuilder.() -> ShapedRecipeBuilder) {
+inline fun RecipeOutput.shaped(
+    item: ItemLike,
+    count: Int,
+    crossinline f: ShapedRecipeBuilder.() -> ShapedRecipeBuilder
+) {
     val b = ShapedRecipeBuilder.shaped(RecipeCategory.MISC, item, count)
     f(b).save(this)
 }
 
-inline fun RecipeOutput.shapeless(item: ItemLike, count: Int, crossinline f: ShapelessRecipeBuilder.() -> ShapelessRecipeBuilder) {
+inline fun RecipeOutput.shapeless(
+    item: ItemLike,
+    count: Int,
+    crossinline f: ShapelessRecipeBuilder.() -> ShapelessRecipeBuilder
+) {
     val b = ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, item, count)
     f(b).save(this)
+}
+
+open class PressingRecipeGenKt(out: PackOutput, registries: CompletableFuture<HolderLookup.Provider>, defaultNamespace: String) :
+    PressingRecipeGen(
+        out,
+        registries,
+        defaultNamespace,
+    ) {
+
+    fun create(name: String, f: StandardProcessingRecipe.Builder<PressingRecipe>.() -> StandardProcessingRecipe.Builder<PressingRecipe>): GeneratedRecipe =
+        create(name, { b -> b.f() })
+}
+
+open class SequencedAssemblyRecipeGenKt(
+    out: PackOutput,
+    registries: CompletableFuture<HolderLookup.Provider>,
+    defaultNamespace: String
+) :
+    SequencedAssemblyRecipeGen(out, registries, defaultNamespace) {
+    fun create(name: String, f: SequencedAssemblyRecipeBuilder.() -> SequencedAssemblyRecipeBuilder): GeneratedRecipe =
+        create(name, { b -> b.f() })
+
+    fun SequencedAssemblyRecipeBuilder.addDeployerStep(item: ItemLike): SequencedAssemblyRecipeBuilder =
+        addStep(
+            ItemApplicationRecipe.Factory { params -> DeployerApplicationRecipe(params) },
+            { it.require(item) },
+        )
+
+    fun SequencedAssemblyRecipeBuilder.addDeployerStep(item: TagKey<Item>): SequencedAssemblyRecipeBuilder =
+        addStep(
+            ItemApplicationRecipe.Factory { params -> DeployerApplicationRecipe(params) },
+            { it.require(item) },
+        )
+
+    fun SequencedAssemblyRecipeBuilder.addPressStep(): SequencedAssemblyRecipeBuilder =
+        addStep(StandardProcessingRecipe.Factory { params -> PressingRecipe(params) }, { it })
 }
